6db203ee7c4de6a15e4c11032026949238088889
charliedunne <charliedunne@gmail.com>
2022-09-26 16:22:16 +0200

    Update model with some custom format

--- db/models.py
+++ db/models.py
@@ -87,6 +87,71 @@ class Card(models.Model):
     def __str__(self):
         return self.code
 
+    def getColorBg(self):
+        match self.faction.short:
+            case "baratheon":
+                return "rgb(230, 200, 50)"
+            case "lannister":
+                return "rgb(230, 30, 30)"
+            case "targaryen":
+                return "black"
+            case "martell":
+                return "orange"
+            case "tyrell":
+                return "green"
+            case "thenightswatch":
+                return "grey"
+            case "greyjoy":
+                return "rgb(60, 50, 200)"
+            case "stark":
+                return "white"
+            case "neutral":
+                return "brown"
+            case _:
+                return ""
+
+    def getColorFg(self):
+        match self.faction.short:
+            case "baratheon":
+                return "black"
+            case "lannister":
+                return "black"
+            case "targaryen":
+                return "White"
+            case "martell":
+                return "black"
+            case "tyrell":
+                return "black"
+            case "thenightswatch":
+                return "black"
+            case "greyjoy":
+                return "white"
+            case "stark":
+                return "black"
+            case "neutral":
+                return "black"
+            case _:
+                return ""
+
+
+    def isCharacter(self):
+        if self.type.name == "Character":
+            return True
+        else:
+            return False
+
+    def isPlot(self):
+        if self.type.name == "Plot":
+            return True
+        else:
+            return False
+    
+    def isLocation(self):
+        if self.type.name == "Location":
+            return True
+        else:
+            return False
+
     def getTraits_es(self):
         lg = Language.objects.all().get(short='es')
         
@@ -152,6 +217,34 @@ class Card(models.Model):
             name = self.name
 
         return name
+
+    def getText_es(self):
+        lg = Language.objects.all().get(short='es')
+        try:
+            translation = TranslateCard.objects.all().get(card=self, language=lg)
+        except:
+            pass
+
+        if 'translation' in locals():
+            text = translation.text
+        else:
+            text = self.name + '*'
+
+        return text
+
+    def getFlavor_es(self):
+        lg = Language.objects.all().get(short='es')
+        try:
+            translation = TranslateCard.objects.all().get(card=self, language=lg)
+        except:
+            pass
+
+        if 'translation' in locals():
+            flavor = translation.flavor
+        else:
+            flavor = self.name + '*'
+
+        return flavor 
 
     @register.simple_tag
     def theName(card, lang):
